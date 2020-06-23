7eb101301f2c8176b8183c18c29e60f78fbe70a6
tomas cisneros pozzi <tomasmate2405@gmail.com>
2020-06-23 12:11:44 -0300

    el tomi de caseros
    cambie el codigo

--- CambiodeNombre.c
+++ CambiodeNombre.c
@@ -3,106 +3,109 @@
 
 int main()
 {
-    int codigoVendedor[3], vendedoresMax[3], i, j = 0, k, x, j2 = 0;
-
-    float cantVentaDinero[3], cantDinero[3];
+    int codigoProducto[3], stockProducto[3], i = 0, cod, cant, k , x, j=0, aux,pmv[3];
+    char tipoOperacion;
 
     for(i = 0; i < 3; i++)
     {
-        printf("Ingrese codigo de vendedor: ");
-        scanf("%d", &codigoVendedor[i]);
-
-        printf("Ingrese la cantidad de dinero de ese vendedor: ");
-        scanf("%f", &cantDinero[i]);
+        printf("Ingrese el codigo del producto: ");
+        scanf("%d", &codigoProducto[i]);
+        printf("Ingrese el stock de ese producto: ");
+        scanf("%d", &stockProducto[i]);
     }
 
-    int cod;
-    printf("Ingrese el codigo de vendedor a trabajar: ");
-    scanf("%d", &cod);
 
-    while(cod != 0)
-    {
-        float precioProducto, cantPlata = 0;
-        int cantidadVendida;
-
-        printf("Ingrese el precio unitario del producto: ");
-        scanf("%f", &precioProducto);
-
-        printf("Ingrese la cantidad vendida de ese producto: ");
-        scanf("%d", &cantidadVendida);
-
-        cantPlata = cantidadVendida * precioProducto;
-
-        for(i = 0; i < 3; i++)
-        {
-            if(codigoVendedor[i] == cod)
-            {
-                cantVentaDinero[j2] = cantPlata;
-                j2++;
-
-                if(cantidadVendida > 300)
-                {
-                    vendedoresMax[j] = cod;
-                    j++;
-                }
-
-                i = 3;
-
-            }
-        }
-
-        printf("Ingrese el codigo de vendedor a trabajar: ");
-        scanf("%d", &cod);
-    }
-
+    //ordenamiento
     k=0;
     x=3;
-
-    int aux;
-    float auxF;
-
     while(k==0)
     {
         x--;
         k=1;
         for(i=0;i<x;i++)
         {
-            if(cantVentaDinero[i]<cantVentaDinero[i+1])
+            if(codigoProducto[i]>codigoProducto[i+1])
             {
+                aux = codigoProducto[i];
+                codigoProducto[i] = codigoProducto[i+1];
+                codigoProducto[i+1] = aux;
 
-
-                auxF = cantDinero[i];
-                cantDinero[i] = cantDinero[i+1];
-                cantDinero[i+1] = auxF;
-
-                aux = codigoVendedor[i];
-                codigoVendedor[i] = codigoVendedor[i+1];
-                codigoVendedor[i+1] = aux;
-
-                auxF = cantVentaDinero[i];
-                cantVentaDinero[i] = cantVentaDinero[i+1];
-                cantVentaDinero[i+1] = auxF;
+                aux = stockProducto[i];
+                stockProducto[i] = stockProducto[i+1];
+                stockProducto[i+1] = aux;
 
                 k=0;
             }
         }
     }
 
+    //busqueda y actualizacion
+    printf("Ingrese el codigo de producto a trabajar (0 para finalizar): ");
+    scanf("%d", &cod);
 
-    //Print
+    while(cod != 0)
+    {
+        int pos;
+        int verificacion = 0;
+        for(i = 0; i < 3; i++)
+        {
+            if(codigoProducto[i] == cod)
+            {
+                pos = i;
+                verificacion = 1;
+            }
+        }
+
+        if(verificacion == 0)
+        {
+            printf("Codigo no encontrado, ");
+        }
+        else
+        {
+            printf("Ingrese el tipo de operacion (e para entrada, s para salida): ");
+            fflush(stdin);
+            scanf("%c", &tipoOperacion);
+            printf("Ingrese la cantidad a trabajar: ");
+            scanf("%d", &cant);
+
+            switch(tipoOperacion)
+            {
+            case 'e':
+                stockProducto[pos] += cant;
+                break;
+            case 's':
+                stockProducto[pos] -= cant;
+                break;
+            default:
+                printf("Tipo de operacion erronea, ingrese otra: ");
+                fflush(stdin);
+                scanf("%c", &tipoOperacion);
+                break;
+            }
+        }
+
+        printf("Ingrese el codigo de producto a trabajar (0 para finalizar): ");
+        scanf("%d", &cod);
+    }
+   //Creacion de un vector a partir de otro
+    for(i=0;i<3;i++)
+    {
+        if(stockProducto[i]>=20)
+        {
+            pmv[j] = codigoProducto[i];
+            j++;
+        }
+    }
+    //salida de datos
+
     for(i = 0; i < 3; i++)
     {
-        printf("cod: %d \n", codigoVendedor[i]);
-        printf("dinero vendido: %f \n", cantVentaDinero[i]);
-        printf("cantidad de dinero inicial: %f \n", cantDinero[i]);
-
+        printf("Codigo: %d ", codigoProducto[i]);
+        printf("Stock: %d \n", stockProducto[i]);
     }
-
-    printf("vendio mas de 300: ");
-
-    for(i = 0; i < j; i++)
+    for(i=0;i<j;i++)
     {
-        printf("%d \n", vendedoresMax[i]);
+        printf("Los productos con mas de 20 de stock son: %d \n", pmv[i]);
     }
 
     return 0;
